b935dc0d598ad065d38a9c036104303b9de3246d
Evan Conrad <evan@roomservice.dev>
2022-03-31 15:38:24 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strangemood IPFS
+
+This is a small IPFS gateway that's setup in Fly. It lives at https://ipfs.strangemood.org.
---
+++ proxy/src/index.ts
@@ -0,0 +1,14 @@
+// import httpProxy from "http-proxy";
+// import http from "http";
+
+// const proxy: httpProxy = httpProxy.createProxyServer({
+//   // IPFS
+//   target: "http://localhost:5001",
+// });
+
+// http
+//   .createServer(function (req, res) {
+
+//     res.end()
+//   })
+//   .listen(process.env.PORT || 9000);
